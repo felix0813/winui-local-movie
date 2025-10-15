86fc6e1b02d3138600bacfe6a58b3d54808c0a7a
felix <1306332027@qq.com>
2025-10-15 18:26:55 +0800

    创建项目

---
+++ HomePage.xaml.cs
@@ -0,0 +1,13 @@
+// HomePage.xaml.cs
+using Microsoft.UI.Xaml.Controls;
+
+namespace winui_local_movie
+{
+    public sealed partial class HomePage : UserControl
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
